5dd556cd59fc8503566d01c45891bbf3c8f5cd02
anuoluwa01 <anu.ogunnubi@gmail.com>
2026-05-04 15:48:02 +0100

    update CORS for production

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ import cryptoRoutes from './routes/crypto.js';
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://raogunnubi-crypto-app.netlify.app'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
